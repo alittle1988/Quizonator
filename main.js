69cbfef6c3f0a1376366c377db5f7af684ac407b
drewlittle88 <81585470+alittle1988@users.noreply.github.com>
2022-05-22 00:04:25 -0700

    added 2nd question and changed some style

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 $(document).ready(() => {
 
+
     $('#incorrect-1').on('click', () =>{
-        $('#incorrect-1').addClass('active-1');
-    });
+        $('#incorrect-1').toggleClass('active-1')
+    }).on('click', () => {
+        $('#tryAgain').show();
+    })
 
     $('#incorrect-2').on('click', () => {
-        $('#incorrect-2').addClass('active-1');
-    });
+        $('#incorrect-2').toggleClass('active-1');
+    }).on('click', () => {
+        $('#tryAgain').show();
+    })
+
     $('#incorrect-3').on('click', () => {
-        $('#incorrect-3').addClass('active-1');
+        $('#incorrect-3').toggleClass('active-1');
+    }).on('click', () => {
+        $('#tryAgain').show();
     });
 
     $('#correct').on('click', () => {
-        $('#correct').addClass('active-2')
+        $('#correct').toggleClass('active-2')
+    }).on('click', () => {
+        $('#goodJob').show()
+    }).on('click', () => {
+        $('#tryAgain').hide()
+    }).on('click', () => {
+        $('#question1').fadeOut(1000)
+    }).on('click', () => {
+        $('#question2').delay(1000).fadeIn(500)
     })
 
 
