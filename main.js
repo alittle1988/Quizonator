9767fe86bfe34d543d7209ccff1f2d04c8e28422
drewlittle88 <81585470+alittle1988@users.noreply.github.com>
2022-05-22 08:46:33 -0700

    changed id to classes

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
 $(document).ready(() => {
 
 
-    $('#incorrect-1').on('click', () =>{
-        $('#incorrect-1').toggleClass('active-1')
+    $('.incorrect-1').on('click', () =>{
+        $('.incorrect-1').toggleClass('active-1')
     }).on('click', () => {
-        $('#tryAgain').show();
+        $('.tryAgain').show();
     })
 
-    $('#incorrect-2').on('click', () => {
-        $('#incorrect-2').toggleClass('active-1');
+    $('.incorrect-2').on('click', () => {
+        $('.incorrect-2').toggleClass('active-1');
     }).on('click', () => {
-        $('#tryAgain').show();
+        $('.tryAgain').show();
     })
 
-    $('#incorrect-3').on('click', () => {
-        $('#incorrect-3').toggleClass('active-1');
+    $('.incorrect-3').on('click', () => {
+        $('.incorrect-3').toggleClass('active-1');
     }).on('click', () => {
-        $('#tryAgain').show();
+        $('.tryAgain').show();
     });
 
-    $('#correct').on('click', () => {
-        $('#correct').toggleClass('active-2')
+    $('.correct').on('click', () => {
+        $('.correct').toggleClass('active-2')
     }).on('click', () => {
-        $('#goodJob').show()
+        $('.goodJob').show()
     }).on('click', () => {
-        $('#tryAgain').hide()
-    }).on('click', () => {
-        $('#question1').fadeOut(1000)
-    }).on('click', () => {
-        $('#question2').delay(1000).fadeIn(500)
+        $('.tryAgain').hide()
     })
+
+    $('.nextQuest').on('click', () => {
+        $('.question1').hide()
+    }).on('click', () => {
+        $('.question2').delay(1000).css('display', 'flex')
+    }).on('click', () => {
+        $('.incorrect-1').removeClass('active-1')
+    }).on('click', () => {
+        $('.incorrect-2').removeClass('active-1')
+    }).on('click', () => {
+        $('.incorrect-3').removeClass('active-1')
+    }).on('click', () => {
+        $('.correct').removeClass('active-2')
+    }).on('click', () => {
+        $('.goodJob').hide()
+    }).on('click', () => {
+        $('.tryAgain').hide()
+    })
+    
 
 
 });
