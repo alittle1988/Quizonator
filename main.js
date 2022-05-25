ea4740eb38bcdb47c0ebb5ab905d395987c4d8c8
drewlittle88 <81585470+alittle1988@users.noreply.github.com>
2022-05-25 11:50:21 -0700

    added random numbers

--- main.js
+++ main.js
@@ -2,28 +2,37 @@ $(document).ready(() => {
     
     const questionArray = ['What is 2 + 2?', 'What is 3 + 3?','What is 4 + 4?', 'What is 5 + 6?', 'What is 6 + 6?'];
     const answerArray = [4, 6, 8, 11, 12]
-    const wrongAnswer = Math.floor(Math.random() * 19)
+    const wrongAnswer = []
 
-    document.getElementById("question-text").innerHTML = questionArray[0];
-    document.getElementById("correct").innerHTML = answerArray[0];
+    let question = document.getElementById("question-text").innerHTML = questionArray[0];
+    let correct = document.getElementById("correct").innerHTML = answerArray[0];
+    
     
 
-    const getWrongAnswer = () => {
-        return Math.floor(Math.random() * 19)
-    };
+    const getWrongAnswers = () => {
+        while (wrongAnswer.length < 3) {
+        let wrongAnswer1 = Math.floor(Math.random() * 14)
+        wrongAnswer.push(wrongAnswer1)
+    }}
+
+    getWrongAnswers()
 
 
-    document.getElementById('wrongAnswer').innerHTML = getWrongAnswer();
-    document.getElementById('wrongAnswer2').innerHTML = getWrongAnswer();
-    document.getElementById('wrongAnswer3').innerHTML = getWrongAnswer();
-
-    if(document.getElementById('wrongAnswer1') === document.getElementById('correct')) {
-        document.getElementById('wrongAnswer').innerHTML = getWrongAnswer();
-    }
+    document.getElementById('wrongAnswer1').innerHTML = wrongAnswer[0]
+    document.getElementById('wrongAnswer2').innerHTML = wrongAnswer[1]
+    document.getElementById('wrongAnswer3').innerHTML = wrongAnswer[2]
 
 
+    $('#wrongAnswer1').on('click', () => {
+        $('#WrongAnswer1').toggleClass('active');
+    });
 
-    
+
+  
+
+
+
+
 
 
 });
