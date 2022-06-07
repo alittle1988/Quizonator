88514f452e4cc5eb7ea08a13f98e78b65e236144
drewlittle88 <81585470+alittle1988@users.noreply.github.com>
2022-06-06 22:13:34 -0700

    trying to get correct answer to move

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 $(document).ready(() => {
     
-    const questionArray = ['What is 2 + 2?', 'What is 3 + 3?','What is 4 + 4?', 'What is 5 + 5?', 'What is 6 + 6?', 'What is 7 + 7?', 'What is 8 + 8?', 'What is 9 + 9?', 'What is 10 + 10?', 'What is 11 + 11?'];
+    const questionArray = ['What is 2 + 2?', 'What is 3 + 3?','What is 4 + 4?', 'What is 5 + 5?', 'What is 6 + 6?', 'What is 7 + 7?', 'What is 8 + 8?', 'What is 9 + 9?', 'What is 10 + 10?', 'What is 11 + 11?', 'Congrats you finished the Test 🎉'];
     const answerArray = ["4", "6", "8", "10", "12", "14", "16", "18", "20", "22"]
-    const q1WrongAnswer = ['6', '8', '2', '5', '6']
-    const q2WrongAnswer = ['5', '3', '9', '0', '9']
-    const q3WrongAnswer = ['2', '9', '4', '9', '10']
+    const q1WrongAnswer = ['6', '8', '2', '5', '4', '7', '13', '9', '10', '11']
+    const q2WrongAnswer = ['5', '3', '9', '0', '8', '13', '15', '19', '30', '7']
+    const q3WrongAnswer = ['2', '9', '4', '8', '10','11', '12', '16', '15', '21']
    
 
 
     // assigning questions and answers to element ID
     document.getElementById("question-text").innerHTML = questionArray[0];
-    let correctAnswer = document.getElementById("correct-1").innerHTML = answerArray[0];
+    document.getElementById("correct-1").innerHTML = answerArray[0];
     
 
-
-    let wrongAnswer1 = q1WrongAnswer[0]
+    let wrongAnswer1 =  q1WrongAnswer[0]
     let wrongAnswer2 = q2WrongAnswer[0]
     let wrongAnswer3 = q3WrongAnswer[0]
     
@@ -32,27 +31,38 @@ $(document).ready(() => {
     // move answer/question boxes to next array
     const moveToNext = (location, array) => {
         if(document.getElementById(location).innerHTML === array[0]){
-            document.getElementById(location).innerHTML = array[0 + 1];
+            document.getElementById(location).innerHTML = array[1];
         } else if (document.getElementById(location).innerHTML === array[1]) {
-            document.getElementById(location).innerHTML = array[1 + 1];
+            document.getElementById(location).innerHTML = array[2];
         } else if (document.getElementById(location).innerHTML === array[2]) {
-            document.getElementById(location).innerHTML = array[2 + 1];
+            document.getElementById(location).innerHTML = array[3];
         } else if (document.getElementById(location).innerHTML === array[3]) {
-            document.getElementById(location).innerHTML = array[3 + 1];
+            document.getElementById(location).innerHTML = array[4];
         } else if (document.getElementById(location).innerHTML === array[4]) {
-            document.getElementById(location).innerHTML = array[4 + 1];
+            document.getElementById(location).innerHTML = array[5];
         } else if (document.getElementById(location).innerHTML === array[5]) {
-            document.getElementById(location).innerHTML = array[5 + 1];
+            document.getElementById(location).innerHTML = array[6];
         } else if (document.getElementById(location).innerHTML === array[6]) {
-            document.getElementById(location).innerHTML = array[6 + 1];
+            document.getElementById(location).innerHTML = array[7];
         } else if (document.getElementById(location).innerHTML === array[7]) {
-            document.getElementById(location).innerHTML = array[7 + 1];
+            document.getElementById(location).innerHTML = array[8];
         } else if (document.getElementById(location).innerHTML === array[8]) {
-            document.getElementById(location).innerHTML = array[8 + 1];
+            document.getElementById(location).innerHTML = array[9];
         } else if (document.getElementById(location).innerHTML === array[9]) {
-            document.getElementById(location).innerHTML = array[9 + 1];
+            document.getElementById(location).innerHTML = array[10];
         }
+        
     }
+
+    function  moveDiv(old, replace) {
+        old.replaceWith(replace)
+        $('.question').append(old)
+    }
+    /*$('.nextQuest').click(function (evt) {
+        let $div = $(evt.target).closest('.answer');
+        $div.next('.answer').after($div);
+    })*/
+
 
 
  
@@ -89,21 +99,22 @@ $(document).ready(() => {
         $('.incorrect-3').removeClass('active-1');
         $('.correct').removeClass('active-2');
         $('.goodJob').hide()
-        $('.tryAgain').hide() 
-    }).on('click', () => {
-        $('#wrongAnswer1').replaceWith('#correct-1');
-        $('.incorrect-1').replaceWith()
+        $('.tryAgain').hide()
+
     }).on('click', () => {
         moveToNext('correct-1', answerArray);
         moveToNext('question-text', questionArray)
         moveToNext('wrongAnswer1', q1WrongAnswer);
         moveToNext('wrongAnswer2', q2WrongAnswer);
         moveToNext('wrongAnswer3', q3WrongAnswer);
+    }).on('click', (event) => {
+        moveDiv($('.incorrect-1'), $('.correct'))
+        //$('.incorrect-1').next().replaceWith($('.correct')).after($('.incorrect-1'))
+       
+        
     })
 
     
-
-
 
 
 });
