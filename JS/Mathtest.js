5c020a2689657e33123d08f23a2b0fd9ba2190c9
Andrew <andylittle88@gmail.com>
2022-07-19 23:25:07 -0700

    created random questions array onbutton click

--- JS/Mathtest.js
+++ JS/Mathtest.js
@@ -1,9 +1,14 @@
 // declaring arrays
-let qArray = ['6 + 5 = ?', '12 + 8  = ?', '3 + 4  = ?', '4 - 8  = ?', '5 - 5  = ?', '6 + 8  = ?', ' 7 + 7  = ?', '9 - 3  = ?', ' 9 + 10  = ?', '11 + 12  = ?'];
+let qArray = ['6 + 5 = ?', '12 + 8  = ?', '3 + 4  = ?', '8 - 4 = ?', '5 - 5  = ?', '6 + 8  = ?', ' 7 + 7  = ?', '9 - 3  = ?', ' 9 + 10  = ?', '11 + 12  = ?'];
+let qArray2 = [];
 let answerArray = ['11', '20', '7', '4', '0', '14', '14', '6', '19', '23'];
+let answerArray2 =[];
 let wrongAnswer1 = ['10', '18', '10', '8', '5', '16', '10', '3', '14', '20'];
+let wAnswer1 = [];
 let wrongAnswer2 = ['15', '12', '5', '6', '2', '10', '12', '9', '17', '13'];
+let wAnswer2 = [];
 let wrongAnswer3 = ['12', '19', '4', '5', '10', '12', '0', '11', '16', '21'];
+let wAnswer3 = [];
 let index = 0;
 
 // declaring varibles for location by class
@@ -13,14 +18,40 @@ let wA1 = document.querySelector('.incorrect-1')
 let wA2 = document.querySelector('.incorrect-2');
 let wA3 = document.querySelector('.incorrect-3');
 
+function makingNewArray() {
+    for(let i = 0; i < 5; i ++) {
+     randNumb = Math.floor(Math.random() * qArray.length);
+     qArray2.push(qArray[randNumb]);
+     answerArray2.push(answerArray[randNumb]);
+     wAnswer1.push(wrongAnswer1[randNumb]);
+     wAnswer2.push(wrongAnswer2[randNumb]);
+     wAnswer3.push(wrongAnswer3[randNumb]);
+     qArray.splice(randNumb, 1);
+     answerArray.splice(randNumb, 1);
+     wrongAnswer1.splice(randNumb, 1);
+     wrongAnswer2.splice(randNumb, 1);
+     wrongAnswer3.splice(randNumb, 1);
+     
+    };
+    document.querySelector('#questionDiv').style.display = 'block';
+    document.querySelector('.buttonDiv').style.display = 'none';
+    document.querySelector('.disclaimer').style.display = 'flex';
+    question.textContent = qArray2[index];
+    a1.textContent = answerArray2[index];
+    wA1.textContent = wAnswer1[index];
+    wA2.textContent = wAnswer2[index];
+    wA3.textContent = wAnswer3[index];
+    
+ }
+
 
 
 // assigning content location to array indexes 
-question.textContent = qArray[index]
-a1.textContent = answerArray[index]
-wA1.textContent = wrongAnswer1[index];
-wA2.textContent = wrongAnswer2[index];
-wA3.textContent = wrongAnswer3[index];
+//question.textContent = qArray2[index]
+//a1.textContent = answerArray2[index]
+//wA1.textContent = wAnswer1[index];
+//wA2.textContent = wAnswer2[index];
+//wA3.textContent = wAnswer3[index];
 
 // declaring score varaibles and display location by class
 let score = 0;
@@ -85,11 +116,11 @@ function moveToNext() {
        
     } else {
         index = index + 1;
-        question.textContent = qArray[index];
-        a1.textContent = answerArray[index];
-        wA1.textContent = wrongAnswer1[index];
-        wA2.textContent = wrongAnswer2[index];
-        wA3.textContent = wrongAnswer3[index];
+        question.textContent = qArray2[index];
+        a1.textContent = answerArray2[index];
+        wA1.textContent = wAnswer1[index];
+        wA2.textContent = wAnswer2[index];
+        wA3.textContent = wAnswer3[index];
         clearDiv(a1, wA1, wA2, wA3);
         rotateDiv()
     }
@@ -119,4 +150,5 @@ function rotateDiv() {
     
     
 }
+
 
