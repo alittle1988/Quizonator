e4d83b243ab850d597a4818045a1c89b6b915201
Andrew <andylittle88@gmail.com>
2022-08-09 11:58:41 -0700

    added eventListener to starttest button

--- JS/additionTestK.js
+++ JS/additionTestK.js
@@ -117,6 +117,10 @@ function rotateDiv() {
 
 
 /* creating Arrays */
+const startTest = document.querySelector('.button')
+startTest.addEventListener('click', function(){
+    createArray()
+})
 function createArray() {
     for(let i = 0;i < 2; i++) {
         for(let j = 0; j < 10; j++) {
